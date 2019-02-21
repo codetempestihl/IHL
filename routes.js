af505f591a4a7291482a819baa44c68c69ed8125
codetempestihl <47829960+codetempestihl@users.noreply.github.com>
2019-02-21 22:07:50 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,13 +1,25 @@
 var express = require('express')
 var router = express.Router()
+var passwordhash=require('password-hash');
 
 router.get('/', function(req, res){
     res.render('index');
 })
 
 router.post('/', function(req, res){
-    console.log(req.body);
     // hanlde signup and login process
+	if (req.body.signup=='true')
+	{
+		var email=req.body.email;
+		var pass=String(req.body.password[0]);
+		//uses bscrypt or scrypt
+		var hashedpassword=passwordhash.generate(pass);
+		
+	}
+	else
+	{
+		console.log('for login');
+	}
 })
 
 module.exports = router

b14abd105ed258752ea0d4cc062b0139cb6bfe58
Sahil <snishal33@gmail.com>
2019-02-21 12:54:05 +0530

    facebook login

--- routes.js
+++ routes.js
@@ -5,4 +5,9 @@ router.get('/', function(req, res){
     res.render('index');
 })
 
+router.post('/', function(req, res){
+    console.log(req.body);
+    // hanlde signup and login process
+})
+
 module.exports = router

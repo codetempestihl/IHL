f6b4b0a9f1504fd34ae745d754ce2fb6c14a740c
codetempestihl <47829960+codetempestihl@users.noreply.github.com>
2019-02-24 15:24:53 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,42 @@
 var express = require('express')
 var router = express.Router()
+var passwordhash=require('password-hash');
+var session=require('express-session');
 
+
+router.use(session({secret:'key'
+,saveUninitialized:false ,resave:true}));
+const redirecthome=(req,res,next)=>{
+	if(req.session.userid)
+	{
+		res.redirect('/home');
+	}
+	else{
+		next();
+	}
+}
+const redirectlogin=(req,res,next)=>{
+	if(!req.session.userid)
+	{
+		res.redirect('/');
+	}
+	else{
+		next();
+	}
+}
 //setup mongoose
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/ihl", { useNewUrlParser: true });
-
+var db=mongoose.connection;
+db.once('open',()=>{
+	console.log("connected");
+});
 //MongoDb schema and model
-var userSchema = new mongoose.Schema({ name: String, fitbit: String });
+var userSchema = new mongoose.Schema({
+	email:{type: String,required :true},
+	password:{type:String,required:true},
+	fblinked:Boolean });
+	
 var User = mongoose.model('User', userSchema);
 
 const FitbitApiClient = require("fitbit-node");
@@ -16,18 +46,72 @@ const client = new FitbitApiClient({
     apiVersion: '1.2' // 1.2 is the default
 });
 
-router.get('/', function(req, res){
+router.get('/',redirecthome, function(req, res){
     res.render('index');
+	
 })
 
 router.post('/', function(req, res){
-    console.log(req.body);
+    
     // hanlde signup and login process
+	if (req.body.signup=='true')
+	{
+		 var pass=String(req.body.password[0]);
+		 hashedpassword=passwordhash.generate(pass);
+		 User.find({ email: req.body.email }, function(err, user){
+			 if (err) throw err;
+			 
+			 if (Object.keys(user).length==0){
+				 console.log("hey");
+					userdata=new User({
+					email:req.body.email,
+					password:hashedpassword,
+					fblinked:true	
+				});
+				userdata.save(function(err){
+					if(err) throw err; 
+					console.log("user created");
+				});
+				const user=User.find({email:req.body.email});
+				req.session.userid=1;
+				res.redirect('/home');
+				}
+				else
+				{
+					console.log("user already exist");
+				}
+		 });
+		 
+	}
+	else if(req.body.login=="true")
+	{
+		User.find({email:req.body.email},function(err,user){
+			if (err) throw err;
+			if (Object.keys(user).length==0)
+			{console.log("user doessnt exist sign up please");}
+			else if( user.fblink!=true)
+			{	user.fblink=true;
+				console.log(user,"logged in");
+				req.session.userid=2;
+				console.log(user,"logged in");
+				res.redirect('/home');}
+			else
+			{
+				req.session.userid=2;
+				console.log(user,"logged in");
+				res.redirect('/home');
+			}
+		})
+	}
 })
 
-router.get('/home', function(req, res){
-    res.render('home');
+router.get('/home',redirectlogin, function(req, res){
+	res.render('home');
 })
+router.get('/logout',function(req,res){
+	req.session.destroy();
+	res.redirect('/');
+});
 
 router.get('/fitbit', function(req, res){
     res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:8080/callback'));
@@ -47,5 +131,5 @@ router.get("/callback", (req, res) => {
 		res.status(err.status).send(err);
 	});
 });
-
-module.exports = router
+module.exports = User;
+module.exports = router;
